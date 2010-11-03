09ad6b323b2b9a74385128e287c5f56046f6e26b
Kent Fredric <kentfredric@gmail.com>
2010-11-03 17:59:57 +1300

    critic tidyup

--- lib/Dist/Zilla/Util/Test/KENTNL.pm
+++ lib/Dist/Zilla/Util/Test/KENTNL.pm
@@ -7,14 +7,73 @@ package Dist::Zilla::Util::Test::KENTNL;
 use Try::Tiny;
 use Dist::Zilla::Tester qw( Builder );
 use Params::Util qw(_HASH0);
+use Moose::Autobox;
 
 use Sub::Exporter -setup => {
   exports => [
     test_config =>
     simple_ini  => \'_simple_ini',
+    dist_ini => '\_dist_ini',
   ],
   groups => [ default => [ qw( -all ) ] ]
 };
+
+=method test_config
+
+This is pretty much why this module exists. Its a little perverse, but makes testing WAY easier.
+
+  my $plugin = test_config({
+    dist_root => 'corpus/dist/DZT',
+    ini       => [
+      'GatherDir',
+      [ 'Prereqs' => { 'Test::Simple' => '0.88' } ],
+    ],
+    post_build_callback => sub {
+        my $config = shift;
+        # Handy place to put post-construction test code.
+        die $config->{error} if $config->{error};
+    },
+    find_plugin => 'SomePluginName'
+  });
+
+Additionally, you can add this section
+
+  callback => {
+    method => 'metadata',
+    args   => [],
+    code   => sub {
+      my $data = shift;
+      print "Errors ( if any ) $data->{error} ";
+      dump  $data->{response}; # response from ->metadata
+      $data->{instance}->doMorestuffbyhand();
+      # ok( .... 'good place for a test!' )
+    },
+  }
+
+Generally, I find it easier to do 1-off function wrappers, i.e.:
+
+  sub make_plugin {
+    my @args = @_;
+    return test_config({
+        dist_root => 'corpus/dist/DZT',
+        ini => [
+          'GatherDir',
+          [ 'Prereqs' => {'Test::Simple' => '0.88' } ],
+          [ 'FakePlugin' => {@args } ],
+        ],
+        post_build_callback => sub {
+          my $config = shift;
+          die $config->{error} if $config->{error};
+        },
+        find_plugin => 'FakePlugin',
+    });
+  }
+
+Which lets us do
+
+  ok( make_plugin( inherit_version => 1 )->inherit_version , 'inherit_verion = 1 propagates' );
+
+=cut
 
 sub test_config {
   my ( $conf ) = shift;
@@ -25,12 +84,12 @@ sub test_config {
   if ( $conf->{ini} ){
     $args->[1] ||= {};
     $args->[1]->{add_files} ||= {};
-    $args->[1]->{add_files}->{'source/dist.ini'} = _simple_ini()->( @{ $conf->{ini} } );
+    $args->[1]->{add_files}->{'source/dist.ini'} = _simple_ini()->( $conf->{ini}->flatten );
   }
   my $build_error = undef;
   my $instance;
   try {
-    $instance = Builder->from_config( @$args );
+    $instance = Builder->from_config( $args->flatten );
 
     if ( $conf->{build} ){
       $instance->build();
@@ -51,11 +110,11 @@ sub test_config {
     if ( $conf->{callback} ){
       my $error = undef;
       my $method = $conf->{callback}->{method};
-      my $args   = $conf->{callback}->{args};
+      my $callargs   = $conf->{callback}->{args};
       my $call   = $conf->{callback}->{code};
       my $response;
       try {
-        $response = $instance->$method( @$args );
+        $response = $instance->$method( $callargs->flatten );
       } catch {
         $error = $_;
       };
@@ -74,40 +133,42 @@ sub _build_ini_builder {
   my ($starting_core) = @_;
   $starting_core ||= {};
 
-  sub {
+  return sub {
     my (@arg) = @_;
     my $new_core = _HASH0($arg[0]) ? shift(@arg) : {};
 
-    my $core_config = { %$starting_core, %$new_core };
+    my $core_config = { $starting_core->flatten, $new_core->flatten };
 
-    my $config = '';
+    my $config = q{};
 
-    for my $key (keys %$core_config) {
-      my @values = ref $core_config->{ $key }
-                 ? @{ $core_config->{ $key } }
-                 : $core_config->{ $key };
+    for my $key ($core_config->keys) {
+      my @values = ref $core_config->at( $key )
+                 ? $core_config->at( $key )->flatten
+                 : $core_config->at( $key );
 
-      $config .= "$key = $_\n" for grep {defined} @values;
+      $config .= "$key = $_\n" for @values->grep(sub{ defined });
     }
 
     $config .= "\n" if length $config;
 
     for my $line (@arg) {
-      my @plugin = ref $line ? @$line : ($line, {});
+      my @plugin = ref $line ? $line->flatten : ($line, {});
       my $moniker = shift @plugin;
       my $name    = _HASH0($plugin[0]) ? undef : shift @plugin;
       my $payload = shift(@plugin) || {};
-
-      die "TOO MANY ARGS TO PLUGIN GAHLGHALAGH" if @plugin;
+      if( @plugin ){
+        require Carp;
+        Carp::croak(q{TOO MANY ARGS TO PLUGIN GAHLGHALAGH});
+      }
 
       $config .= '[' . $moniker;
       $config .= ' / ' . $name if defined $name;
       $config .= "]\n";
 
-      for my $key (keys %$payload) {
-        my @values = ref $payload->{ $key }
-                   ? @{ $payload->{ $key } }
-                   : $payload->{ $key };
+      for my $key ($payload->keys) {
+        my @values = ref $payload->at( $key )
+                   ?  $payload->at( $key )->flatten
+                   : $payload->at( $key );
 
         $config .= "$key = $_\n" for @values;
       }
@@ -119,12 +180,13 @@ sub _build_ini_builder {
   }
 }
 
+## no critic (Subroutines::ProhibitUnusedPrivateSubroutines)
 sub _dist_ini {
-  _build_ini_builder;
+  return _build_ini_builder;
 }
 
 sub _simple_ini {
-  _build_ini_builder({
+  return _build_ini_builder({
     name     => 'DZT-Sample',
     abstract => 'Sample DZ Dist',
     version  => '0.001',
