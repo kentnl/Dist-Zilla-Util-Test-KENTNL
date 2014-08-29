b9a7205595ba9c765d97e3a5041f4691a5caf307
Kent Fredric <kentfredric@gmail.com>
2014-08-29 19:47:17 +1200

    Build results of f37ae39 (on master)

--- lib/Dist/Zilla/Util/Test/KENTNL/dztest.pm
+++ lib/Dist/Zilla/Util/Test/KENTNL/dztest.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Util::Test::KENTNL::dztest;
 
-our $VERSION = '1.003000';
+our $VERSION = '1.003001';
 
 # ABSTRACT: Shared dist testing logic for easy dzil things
 
@@ -525,7 +525,7 @@ Dist::Zilla::Util::Test::KENTNL::dztest - Shared dist testing logic for easy dzi
 
 =head1 VERSION
 
-version 1.003000
+version 1.003001
 
 =head1 SYNOPSIS
 
