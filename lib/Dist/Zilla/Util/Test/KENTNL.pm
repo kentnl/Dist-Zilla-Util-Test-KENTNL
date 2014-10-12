d527634cdb27072581d81bbfb8ce400d6ea42430
Kent Fredric <kentfredric@gmail.com>
2014-10-13 04:45:27 +1300

    v1.004000
     [Dependencies::Stats]
     - Dependencies changed since 1.003003, see misc/*.deps* for details
     - develop: +3 ↑1 (suggests: ↑1)
     - runtime: +1
     - test: +1
    
     [Features]
     - dztest now has ->run_command() proxy for DZ:App::Tester::test_dzil

--- lib/Dist/Zilla/Util/Test/KENTNL.pm
+++ lib/Dist/Zilla/Util/Test/KENTNL.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Util::Test::KENTNL;
 
-our $VERSION = '1.003004';
+our $VERSION = '1.004001';
 
 #ABSTRACT: KENTNL's DZil plugin testing tool
 
--- lib/Dist/Zilla/Util/Test/KENTNL/dztest.pm
+++ lib/Dist/Zilla/Util/Test/KENTNL/dztest.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Util::Test::KENTNL::dztest;
 
-our $VERSION = '1.003004';
+our $VERSION = '1.004001';
 
 # ABSTRACT: Shared dist testing logic for easy dzil things
 
