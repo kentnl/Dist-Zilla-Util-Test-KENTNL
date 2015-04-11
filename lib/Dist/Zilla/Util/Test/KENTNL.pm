f24afa9291afd3ff3cfbdc7708fa5c3b5dfa9339
Kent Fredric <kentfredric@gmail.com>
2015-04-11 21:00:20 +1200

    Defer more loading

--- lib/Dist/Zilla/Util/Test/KENTNL.pm
+++ lib/Dist/Zilla/Util/Test/KENTNL.pm
@@ -16,7 +16,6 @@ use Sub::Exporter -setup => {
   exports => [ 'test_config', 'dztest' ],
   groups => [ default => [qw( -all )] ],
 };
-use Test::DZil qw(simple_ini);
 
 =method C<dztest>
 
@@ -132,7 +131,10 @@ sub test_config {
   if ( $conf->{ini} ) {
     $args->[1] ||= {};
     $args->[1]->{add_files} ||= {};
-    $args->[1]->{add_files}->{'source/dist.ini'} = simple_ini( @{ $conf->{ini} } );
+    require Test::DZil;
+    ## no critic (Subroutines::ProhibitCallsToUnexportedSubs)
+    ## no critic (Subroutines::ProtectPrivateSubs)
+    $args->[1]->{add_files}->{'source/dist.ini'} = Test::DZil::_simple_ini()->( @{ $conf->{ini} } );
   }
   my $build_error = undef;
   my $instance;
