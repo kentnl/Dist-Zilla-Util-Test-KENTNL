b8cf5053416b12d3276a375b33d85e7c31968a77
Kent Fredric <kentfredric@gmail.com>
2015-04-11 21:12:38 +1200

    Build results of 3c4e4d9 (on master)

--- lib/Dist/Zilla/Util/Test/KENTNL/dztest.pm
+++ lib/Dist/Zilla/Util/Test/KENTNL/dztest.pm
@@ -106,6 +106,7 @@ sub _subtest_prereqs_deeply {
     Test::Differences::eq_or_diff( $meta->{prereqs}, $prereqs, 'Prereqs match expected set' );
   }
   else {
+    ## no critic (Subroutines::ProhibitCallsToUnexportedSubs)
     Test::More::is_deeply( $meta->{prereqs}, $prereqs, 'Prereqs match expected set' );
   }
   return;
@@ -213,6 +214,7 @@ EOF
     Test::Differences::eq_or_diff( \@results, $expected, 'distmeta matched expectations' );
   }
   else {
+    ## no critic (Subroutines::ProhibitCallsToUnexportedSubs)
     Test::More::is_deeply( \@results, $expected, 'distmeta matched expectations' );
   }
   return;
@@ -416,7 +418,7 @@ has configure => (
 sub _build_configure {
   my ($self) = @_;
   require Dist::Zilla::Tester;
-  my $b = Dist::Zilla::Tester::builder()->from_config( { dist_root => q[] . $self->tempdir } );
+  my $b = Dist::Zilla::Tester->builder()->from_config( { dist_root => q[] . $self->tempdir } );
   return $b;
 }
 
