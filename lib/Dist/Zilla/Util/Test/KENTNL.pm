6bfc26ffa52bd99de59821e3ba667c0e6b0fa894
Kent Fredric <kentfredric@gmail.com>
2015-04-13 15:38:54 +1200

    Build results of 10a266e (on master)

--- lib/Dist/Zilla/Util/Test/KENTNL.pm
+++ lib/Dist/Zilla/Util/Test/KENTNL.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Util::Test::KENTNL;
 
-our $VERSION = '1.005012';
+our $VERSION = '1.005013';
 
 #ABSTRACT: KENTNL's DZil plugin testing tool
 
@@ -211,7 +211,7 @@ Dist::Zilla::Util::Test::KENTNL - KENTNL's DZil plugin testing tool
 
 =head1 VERSION
 
-version 1.005012
+version 1.005013
 
 =head1 DESCRIPTION
 
